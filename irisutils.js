bd9c3bbc14da4cf3e0d1847caf93fe62aa01716f
irischen0409 <fmp1314@gmail.com>
2019-07-26 13:01:10 +0800

    20190726

--- irisutils.js
+++ irisutils.js
@@ -81,3 +81,17 @@ function checkAB(ans,gus){
     }
     return `${a}A${b}B`;
 }
+
+
+function clone(source){
+    if (source == null || typeof(source) != 'object') return null;
+    let target = new Object();
+    for (let attr in source){
+        if (typeof(source[attr]) != 'object'){
+            target[attr] = source[attr]
+        }else{
+            target[attr] = clone(source[attr]);
+        }
+    }
+    return target;     
+}
